0feef53b05474bc255dcd4558e6e9ebea4521928
Saikat Jana <saikatjana439@gmail.com>
2026-05-21 20:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (87.10%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
+int num (char c)
+{
+    if (c=='I')
+    return 1;
+    else if (c=='V')
+    return 5;
+       else if (c=='X')
+    return 10;
+       else if (c=='L')
+    return 50;
+       else if (c=='C')
+    return 100;
+       else if (c=='D')
+    return 500;
+    else 
+    return 1000;
+
+}
     int romanToInt(string s) {
-        unordered_map<char, int> mp;
+  
 
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
+    int sum =0,index=0;
+    while (index<s.size()-1)
+    {
+        if (num(s[index])<num (s[index+1]))
+        sum-=num(s[index]);
+        else 
+        sum+= num(s[index]);
+        index++;
 
-        int ans = 0;
 
-        for (int i = 0; i < s.size(); i++) {
 
-            // If current value is smaller than next value
-            if (i + 1 < s.size() && mp[s[i]] < mp[s[i + 1]]) {
-                ans -= mp[s[i]];
-            }
-            else {
-                ans += mp[s[i]];
-            }
-        }
-
-        return ans;
     }
+    sum+=num(s[index]);
+    
+    return sum;
+    
+
+    }
+
+
 };
