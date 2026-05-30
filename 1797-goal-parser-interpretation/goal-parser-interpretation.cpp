e160cbec067e7d793ca11a8f36ab51228e4ea096
Saikat Jana <saikatjana439@gmail.com>
2026-05-31 00:46:42 +0530

    Time: 3 ms (40.44%) | Memory: 8.3 MB (14.06%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        for (int i=0;i<command.size();i++)
+        {
+            if (command[i]=='G')
+            {
+                ans+='G';
+            }
+            else if (command[i]=='('&&command[i+1]==')')
+            {
+                ans+='o';
+                i++;
+            }
+            else if (command[i]=='('&& command[i+1]=='a')
+            {
+                ans+="al";
+                i=i+3;
+            }
+        }
+        return ans;
+
+        
+    }
+};
