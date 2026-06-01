fa3dea0fcd274dc426610b679f8c40fa8261382c
Saikat Jana <saikatjana439@gmail.com>
2026-06-02 00:44:48 +0530

    Time: 3 ms (10.94%) | Memory: 8.7 MB (84.04%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<bool> alpha(26, 0);
-
-        for (int i = 0; i < sentence.size(); i++) {
-            alpha[sentence[i] - 'a'] = 1;
+      vector<bool>arr(26,0);
+     for (int i=0;i<sentence.size();i++)
+     {
+        arr[sentence[i]-'a']=1;
+     }
+     for (int i=0;i<26;i++)
+     {
+        if (arr[i]==0)
+        {
+            return 0;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if (alpha[i] == 0)
-                return 0;
-        }
-
-        return 1;
+     }
+     return 1;
+  
     }
 };
+   
