01afa2f7101f8b601685536b6c3c0ee153f99228
Saikat Jana <saikatjana439@gmail.com>
2026-06-08 01:26:03 +0530

    Time: 4 ms (22.81%) | Memory: 11 MB (36.99%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string temp;
+
+        for (char ch : s) {
+            if (ch != ' ') {
+                temp += ch;
+            } 
+            else if (!temp.empty()) {
+                words.push_back(temp);
+                temp.clear();
+            }
+        }
+
+        if (!temp.empty()) {
+            words.push_back(temp);
+        }
+
+        string ans;
+
+        for (int i = words.size() - 1; i >= 0; i--) {
+            ans += words[i];
+
+            if (i != 0) {
+                ans += " ";
+            }
+        }
+
+        return ans;
+    }
+};
