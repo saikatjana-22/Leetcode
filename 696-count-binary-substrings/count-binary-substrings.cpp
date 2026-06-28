333c6548a7b2fbbe67de0f7ecb0d96093ee7bf84
Saikat Jana <saikatjana439@gmail.com>
2026-06-28 10:57:03 +0530

    Time: 11 ms (8.01%) | Memory: 13 MB (55.91%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int prev=0;
-        int curr=1;
-        int ans=0;
-        for (int i=1;i<s.size();i++)
+       int prev=0;
+       int curr=1;
+       int ans =0;
+       for (int i=1;i<s.size();i++)
+       {
+        if (s[i]==s[i-1])
         {
-            if (s[i]==s[i-1])
-            {
-             curr++;
-            }
-            else
-            {
-                ans+=min (prev,curr);
-                prev=curr;
-                curr=1;
-            }
-
+            curr++;
         }
-        return ans+ min (prev,curr); //for loop theke beriye gele baki add korar jonno min (prev,curr);
+        else
+        {
+            ans+=min (prev,curr);
+            prev=curr;
+            curr=1;
+        }
+
+       }
+       return ans+=min (prev,curr); // extra minimum element jodi pai tahole prev and curr jodi pore thake loop er theke beriye gele 
         
     }
 };
