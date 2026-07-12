6abf568be2f5f9ca2275b6ede1d7367c8c020a95
Saikat Jana <saikatjana439@gmail.com>
2026-07-12 21:28:38 +0530

    Time: 291 ms (74.55%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -5,3 +5,14 @@ from
 world
 where 
 area>=3000000 or population>=25000000
+
+--  eta union kore korche alada alada
+-- SELECT name, population, area
+-- FROM World
+-- WHERE area >= 3000000
+
+-- UNION
+
+-- SELECT name, population, area
+-- FROM World
+-- WHERE population >= 25000000;
