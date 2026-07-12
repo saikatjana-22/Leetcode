2946e66b4142fc01e12800de01342d68d46b9abe
Saikat Jana <saikatjana439@gmail.com>
2026-07-13 00:16:54 +0530

    Time: 450 ms (37.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1258-article-views-i/article-views-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+-- rename korar jonno as keyword use hoy r ascending order er jonno order by keyword use hoy 
+SELECT DISTINCT author_id as id
+from 
+Views
+where 
+author_id=viewer_id 
+order by id
