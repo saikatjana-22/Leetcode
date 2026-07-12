b556b340e32d3b35a2701f5377511114247165e3
Saikat Jana <saikatjana439@gmail.com>
2026-07-13 00:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (55.95%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         //  ami eta prefix and suffix sum diyeo korte pari but leftsum right sum er r ekta extra formula ache seta diyeo korte parbo 
-int n =nums.size();
+
+
+        int n = nums.size();
         int totalsum =0;
         for (int i=0;i<n;i++)
         {
-            totalsum = totalsum+nums[i];
+            totalsum+=nums[i];
         }
-
         int leftsum =0;
         for (int i=0;i<n;i++)
         {
-            int rightsum = totalsum-leftsum-nums[i];
-            if (leftsum==rightsum)
-            {
-                return i;
-            }
-            leftsum=leftsum+nums[i];
-        }
+            int rightsum=totalsum-leftsum-nums[i];
 
+
+            if (leftsum==rightsum)
+            return i;
+
+            leftsum+=nums[i];
+        }
         return -1;
-        
+
+
+
+
     }
 };
