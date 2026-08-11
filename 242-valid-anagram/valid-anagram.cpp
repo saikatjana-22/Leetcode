a42c1f277ea00cf7edbfbfeb3c1e5f4529c50c6e
Saikat Jana <saikatjana439@gmail.com>
2026-08-11 20:14:57 +0530

    Time: 1 ms (54.51%) | Memory: 9.8 MB (50.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -26,39 +26,63 @@ public:
 
 
         // 2nd approach hashmap;
-        if (s.length()!=t.length()) 
-          return false; 
-        unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
+        // if (s.length()!=t.length()) 
+        //   return false; 
+        // unordered_map<char,int>m1;
+        // unordered_map<char,int>m2;
+        // for (int i=0;i<s.length();i++)
+        // {
+        //     m1[s[i]]++; //eta direct frequency count korte thake ;
+
+        // }
+        // for (int i=0;i<t.length();i++)
+        // {
+        //     m2[t[i]]++; //eta direct frequency count korte thake ;
+
+        // }
+        // for (auto x :m1)
+
+        // {
+        //     char ch1 = x.first;
+        //     int freq1 =x.second;
+        //     if (m2.find(ch1)!=m2.end())
+        //     {
+        //         int freq2=m2 [ch1];
+        //         if (freq1!=freq2)
+        //         return false;
+
+        //     }
+        //     else 
+        //     {
+        //      return  false;  // jodi element na e pai 
+        //     }
+        // }
+        // return true;
+        
+        // 3rd approach 
+           if (s.length()!=t.length()) 
+             return false; 
+        unordered_map<char,int>map1;
+     
         for (int i=0;i<s.length();i++)
         {
-            m1[s[i]]++; //eta direct frequency count korte thake ;
+            map1[s[i]]++; //eta direct frequency count korte thake ;
 
         }
-        for (int i=0;i<t.length();i++)
+        for (int i =0;i<t.length();i++)
         {
-            m2[t[i]]++; //eta direct frequency count korte thake ;
-
+            char ch =t[i];
+             if (map1.find(ch)!=map1.end())
+             {
+map1[ch]--;
+if (map1[ch]==0) map1.erase(ch);
+             }
+             else return false;
         }
-        for (auto x :m1)
-
-        {
-            char ch1 = x.first;
-            int freq1 =x.second;
-            if (m2.find(ch1)!=m2.end())
-            {
-                int freq2=m2 [ch1];
-                if (freq1!=freq2)
-                return false;
-
-            }
-            else 
-            {
-             return  false;  // jodi element na e pai 
-            }
-        }
+        if (map1.size()>0) return false ;
+          
         return true;
-        
+
 
 
 
