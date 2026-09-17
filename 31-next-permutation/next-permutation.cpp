aa17a00f17dd3250f652c971d2d8962003dbbd89
Saikat Jana <saikatjana439@gmail.com>
2026-09-18 02:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (15.03%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n= nums.size();
+
+        //  first find the new gola 
+        int gola_idx=-1;
+        for (int i= n-1;i>0;i--)
+        {
+        if (nums[i-1]<nums[i])
+        {
+            gola_idx=i-1;
+            break;
+
+        }
+
+
+        }
+        if (gola_idx!=-1)
+        {
+        // gola inedx er  right side e check korbo j kono greater element ache kina gola index er theke 
+          int swap_idx= gola_idx;
+          for (int j= n-1; j>=gola_idx+1;j--)
+          {
+            if (nums[j]>nums[gola_idx])
+            {
+            swap_idx=j;
+            break;
+            }
+          }
+
+       swap (nums[gola_idx], nums[swap_idx]);
+        }
+       reverse (nums.begin()+gola_idx+1,nums.end());
+        
+    }
+};
